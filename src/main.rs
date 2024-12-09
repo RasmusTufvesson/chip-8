412dfdd59bfcc5ada70fb61abfe5dddeaa3c6f2c
RasmusTufvesson <lars@bizzbook.com>
2024-12-09 22:33:59 +0100

    add other instructions and debugging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, time::{Duration, Instant}};
-use sdl2::{event::Event, pixels::Color, rect::Rect};
+use rand::{rngs::ThreadRng, thread_rng, Rng};
+use sdl2::{event::Event, keyboard::Scancode, pixels::Color, rect::Rect};
 
 const SCALE: u32 = 10;
 const WIDTH: u8 = 64;
@@ -24,11 +25,18 @@ const FONT: [u8; 80] = [
     0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
+const SCANCODES: [Scancode; 16] = [
+    Scancode::Num1, Scancode::Num2, Scancode::Num3, Scancode::Num4,
+    Scancode::Q, Scancode::W, Scancode::E, Scancode::R,
+    Scancode::A, Scancode::S, Scancode::D, Scancode::F,
+    Scancode::Z, Scancode::X, Scancode::C, Scancode::V,
+    ];
+
 const BLACK: Color = Color::RGB(0, 0, 0);
 const WHITE: Color = Color::RGB(255, 255, 255);
 
 fn main() {
-    let program = fs::read(r"programs\IBM Logo.ch8").unwrap();
+    let program = fs::read(r"programs\test_opcode.ch8").unwrap();
 
     let mut interpreter = Interpreter {
         ram: RAM::new(program.as_slice()),
@@ -39,13 +47,16 @@ fn main() {
         delay: Timer::new(),
         sound: Timer::new(),
         variable: [0; 16],
+        keypad: Keypad { keys: [false; 16] },
+        rng: thread_rng(),
+
+        original_shift_instruction: false,
+        original_jump_with_offset_instruction: true,
+        original_add_to_index_instruction: true,
+        original_store_and_load_instruction: false,
+
+        debug: false,
     };
-
-    for _ in 0..50 {
-        interpreter.step();
-    }
-
-    // println!("{:?}", interpreter.display.raw);
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -62,6 +73,7 @@ fn main() {
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut last_frame = Instant::now();
+    let mut delta = 0.0;
     'running: loop {
         canvas.set_draw_color(BLACK);
         canvas.clear();
@@ -69,8 +81,43 @@ fn main() {
             match event {
                 Event::Quit {..} => {
                     break 'running
-                },
+                }
+                Event::KeyDown { scancode, .. } => {
+                    if let Some(code) = scancode {
+                        if let Some((index, _)) = SCANCODES.iter().enumerate().find(|(_, x)| x == &&code) {
+                            interpreter.keypad.keys[index] = true;
+                        } else if code == Scancode::Escape {
+                            interpreter.debug = !interpreter.debug;
+                        } else if interpreter.debug {
+                            match code {
+                                Scancode::Space => interpreter.step(),
+                                Scancode::Num0 => println!("{:?}", interpreter.variable),
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+                Event::KeyUp { scancode, .. } => {
+                    if let Some(code) = scancode {
+                        if let Some((index, _)) = SCANCODES.iter().enumerate().find(|(_, x)| x == &&code) {
+                            interpreter.keypad.keys[index] = false;
+                        }
+                    }
+                }
                 _ => {}
+            }
+        }
+
+        if interpreter.delay.raw != 0 {
+            interpreter.delay.raw -= 1;
+        }
+        if interpreter.sound.raw != 0 {
+            interpreter.sound.raw -= 1;
+        }
+
+        if !interpreter.debug {
+            for _ in 0..(delta / (1. / 700.)) as usize {
+                interpreter.step();
             }
         }
         
@@ -84,16 +131,18 @@ fn main() {
         }
 
         canvas.present();
-        let delta = Instant::now() - last_frame;
-        if delta.as_secs_f64() < 1. / 60. {
-            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60) - delta);
+        let diff = Instant::now() - last_frame;
+        if diff.as_secs_f64() < 1. / 60. {
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60) - diff);
         }
+        delta = (Instant::now() - last_frame).as_secs_f64();
         last_frame = Instant::now()
     }
 }
 
 struct RAM {
     raw: [u8; 4096],
+    last_program_address: u16,
 }
 
 impl RAM {
@@ -103,6 +152,7 @@ impl RAM {
         raw[0x200..0x200 + program.len()].copy_from_slice(program);
         Self {
             raw,
+            last_program_address: 0x200 + program.len() as u16,
         }
     }
 
@@ -112,6 +162,10 @@ impl RAM {
 
     fn fetch_u16(&self, address: u16) -> u16 {
         (self.fetch(address) as u16) << 8 | self.fetch(address + 1) as u16
+    }
+
+    fn set(&mut self, address: u16, value: u8) {
+        self.raw[address as usize] = value;
     }
 }
 
@@ -171,6 +225,10 @@ impl Timer {
     }
 }
 
+struct Keypad {
+    keys: [bool; 16]
+}
+
 struct Interpreter {
     ram: RAM,
     display: Display,
@@ -180,65 +238,146 @@ struct Interpreter {
     delay: Timer,
     sound: Timer,
     variable: [u8; 16],
+    keypad: Keypad,
+    rng: ThreadRng,
+    original_shift_instruction: bool,
+    original_jump_with_offset_instruction: bool,
+    original_add_to_index_instruction: bool,
+    original_store_and_load_instruction: bool,
+    debug: bool,
 }
 
 impl Interpreter {
     fn step(&mut self) {
-        let opcode = self.ram.fetch_u16(self.pc);
-        self.pc += 2;
-        self.execute(Instruction {
-            opcode,
-            kind: (opcode & 0b1111000000000000) >> 12,
-            x: ((opcode & 0b0000111100000000) >> 8) as usize,
-            y: ((opcode & 0b0000000011110000) >> 4) as usize,
-            n: (opcode & 0b0000000000001111) as u8,
-            nn: (opcode & 0b0000000011111111) as u8,
-            nnn: opcode & 0b0000111111111111,
-        });
+        if self.pc <= self.ram.last_program_address {
+            let opcode = self.ram.fetch_u16(self.pc);
+            if self.debug {
+                print!("{:#x}: {:#x}: ", self.pc, opcode);
+            }
+            self.pc += 2;
+            self.execute(Instruction {
+                opcode,
+                kind: (opcode & 0b1111000000000000) >> 12,
+                x: ((opcode & 0b0000111100000000) >> 8) as usize,
+                y: ((opcode & 0b0000000011110000) >> 4) as usize,
+                n: (opcode & 0b0000000000001111) as u8,
+                nn: (opcode & 0b0000000011111111) as u8,
+                nnn: opcode & 0b0000111111111111,
+            });
+        }
     }
 
     fn execute(&mut self, instruction: Instruction) {
-        // println!("{:#6x} {:#b} {:#18b}", instruction.opcode, instruction.kind, instruction.opcode & 0b1111000000000000);
         match instruction.kind {
             0 => {
                 match instruction.nnn {
-                    0x0e0 => self.display.raw.fill(false),
-                    0x0ee => self.pc = self.stack.pop(),
-                    _ => {
-                        println!("Machine language instruction '{:#x}'", instruction.opcode);
+                    0x0e0 => {
+                        self.display.raw.fill(false);
+                        if self.debug { println!("clear") }
                     }
+                    0x0ee => {
+                        self.pc = self.stack.pop();
+                        if self.debug { println!("return") }
+                    }
+                    _ => println!("Machine language instruction '{:#x}'", instruction.opcode),
                 }
             }
             1 => {
                 self.pc = instruction.nnn;
+                if self.debug { println!("jump {:#x}", self.pc) }
             }
             2 => {
                 self.stack.push(self.pc);
                 self.pc = instruction.nnn;
+                if self.debug { println!("call {:#x}", self.pc) }
             }
             3 => {
                 if self.variable[instruction.x] == instruction.nn {
                     self.pc += 2;
                 }
+                if self.debug { println!("eq {:#x} {:#x}", instruction.x, instruction.nn) }
             }
             4 => {
                 if self.variable[instruction.x] != instruction.nn {
                     self.pc += 2;
                 }
+                if self.debug { println!("neq {:#x} {:#x}", instruction.x, instruction.nn) }
             }
             5 => {
                 if self.variable[instruction.x] == self.variable[instruction.y] {
                     self.pc += 2;
                 }
+                if self.debug { println!("eqv {:#x} {:#x}", instruction.x, instruction.y) }
             }
             9 => {
                 if self.variable[instruction.x] != self.variable[instruction.y] {
                     self.pc += 2;
                 }
+                if self.debug { println!("neqv {:#x} {:#x}", instruction.x, instruction.y) }
             }
-            6 => self.variable[instruction.x] = instruction.nn,
-            7 => self.variable[instruction.x] += instruction.nn,
-            0xA => self.ir = instruction.nnn,
+            6 => {
+                self.variable[instruction.x] = instruction.nn;
+                if self.debug { println!("setvn {:#x} {:#x}", instruction.x, instruction.nn) }
+            }
+            7 => {
+                (self.variable[instruction.x], _) = self.variable[instruction.x].overflowing_add(instruction.nn);
+                if self.debug { println!("addvn {:#x} {:#x}", instruction.x, instruction.nn) }
+            }
+            8 => {
+                match instruction.n {
+                    0 => self.variable[instruction.x] = self.variable[instruction.y],
+                    1 => self.variable[instruction.x] |= self.variable[instruction.y],
+                    2 => self.variable[instruction.x] &= self.variable[instruction.y],
+                    3 => self.variable[instruction.x] ^= self.variable[instruction.y],
+                    4 => {
+                        let (value, overflow) = self.variable[instruction.x].overflowing_add(self.variable[instruction.y]);
+                        self.variable[0xF] = if overflow { 1 } else { 0 };
+                        self.variable[instruction.x] = value;
+                    }
+                    5 => {
+                        let (value, overflow) = self.variable[instruction.x].overflowing_sub(self.variable[instruction.y]);
+                        self.variable[0xF] = if overflow { 0 } else { 1 };
+                        self.variable[instruction.x] = value;
+                    }
+                    7 => {
+                        let (value, overflow) = self.variable[instruction.y].overflowing_sub(self.variable[instruction.x]);
+                        self.variable[0xF] = if overflow { 0 } else { 1 };
+                        self.variable[instruction.x] = value;
+                    }
+                    6 => {
+                        if self.original_shift_instruction {
+                            self.variable[instruction.x] = self.variable[instruction.y];
+                        }
+                        self.variable[0xF] = self.variable[instruction.x] & 0b00000001;
+                        self.variable[instruction.x] >>= 1;
+                    }
+                    0xE => {
+                        if self.original_shift_instruction {
+                            self.variable[instruction.x] = self.variable[instruction.y];
+                        }
+                        self.variable[0xF] = (self.variable[instruction.x] & 0b10000000) >> 7;
+                        self.variable[instruction.x] <<= 1;
+                    }
+                    _ => println!("Unknown logic/arithmetic instruction '{:#x}'", instruction.opcode),
+                }
+                if self.debug { println!("logic/arithmetic") }
+            }
+            0xA => {
+                self.ir = instruction.nnn;
+                if self.debug { println!("seti {:#x}", instruction.nn) }
+            }
+            0xB => {
+                if self.original_jump_with_offset_instruction {
+                    self.pc = instruction.nnn + self.variable[0] as u16;
+                } else {
+                    self.pc = instruction.nnn + self.variable[instruction.x] as u16;
+                }
+                if self.debug { println!("jumpo {:#x}", instruction.x) }
+            }
+            0xC => {
+                self.variable[instruction.x] = self.rng.gen::<u8>() & instruction.nn;
+                if self.debug { println!("rand {:#x}", instruction.x) }
+            }
             0xD => {
                 let x = self.variable[instruction.x] % WIDTH;
                 let y = self.variable[instruction.y] % HEIGHT;
@@ -254,6 +393,90 @@ impl Interpreter {
                         }
                         self.display.set(x + j, y + i, data & (0b10000000 >> j) != 0, &mut self.variable[0xF]);
                     }
+                }
+                if self.debug { println!("draw {:#x} {:#x} {:#x}", instruction.x, instruction.y, instruction.n) }
+            }
+            0xE => {
+                match instruction.nn {
+                    0x9e => {
+                        if self.keypad.keys[instruction.x] {
+                            self.pc += 2;
+                        }
+                        if self.debug { println!("prsd {:#x}", instruction.x) }
+                    }
+                    0xa1 => {
+                        if !self.keypad.keys[instruction.x] {
+                            self.pc += 2;
+                        }
+                        if self.debug { println!("nprsd {:#x}", instruction.x) }
+                    }
+                    _ => println!("Unknown input instruction '{:#x}'", instruction.opcode),
+                }
+            }
+            0xF => {
+                match instruction.nn {
+                    0x07 => {
+                        self.variable[instruction.x] = self.delay.raw;
+                        if self.debug { println!("setvd") }
+                    }
+                    0x15 => {
+                        self.delay.raw = self.variable[instruction.x];
+                        if self.debug { println!("setdv") }
+                    }
+                    0x18 => {
+                        self.sound.raw = self.variable[instruction.x];
+                        if self.debug { println!("setsv") }
+                    }
+                    0x1e => {
+                        self.ir += self.variable[instruction.x] as u16;
+                        if !self.original_add_to_index_instruction && self.ir & 0b1000 != 0 {
+                            self.variable[0xF] = 1;
+                        }
+                        if self.debug { println!("addiv") }
+                    }
+                    0x0a => {
+                        let mut continue_execution = false;
+                        for k in 0..16 {
+                            if self.keypad.keys[k] {
+                                continue_execution = true;
+                                self.variable[instruction.x] = k as u8;
+                            }
+                        }
+                        if !continue_execution {
+                            self.pc -= 2;
+                        }
+                        if self.debug { println!("wait") }
+                    }
+                    0x29 => {
+                        self.ir = ((self.variable[instruction.x] & 0b00001111) * 5) as u16;
+                        if self.debug { println!("setidgt") }
+                    }
+                    0x33 => {
+                        let number = self.variable[instruction.x];
+                        self.ram.set(self.ir, number / 100);
+                        self.ram.set(self.ir + 1, (number % 100) / 10);
+                        self.ram.set(self.ir + 2, number % 10);
+                        if self.debug { println!("dgt") }
+                    }
+                    0x55 => {
+                        for register in 0..=instruction.x {
+                            self.ram.set(self.ir + register as u16, self.variable[register]);
+                        }
+                        if self.original_store_and_load_instruction {
+                            self.ir += instruction.x as u16 + 1;
+                        }
+                        if self.debug { println!("setmv") }
+                    }
+                    0x65 => {
+                        for register in 0..=instruction.x {
+                            self.variable[register] = self.ram.fetch(self.ir + register as u16);
+                        }
+                        if self.original_store_and_load_instruction {
+                            self.ir += instruction.x as u16 + 1;
+                        }
+                        if self.debug { println!("setvm") }
+                    }
+                    _ => println!("Unknown instruction '{:#x}'", instruction.opcode),
                 }
             }
             _ => println!("Unknown instruction kind '{:#x}'", instruction.opcode),
